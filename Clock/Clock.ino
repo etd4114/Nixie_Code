0fb82caaa48cf4c1546705039defd17080491125
Your Name <etd4sv@virgia.edu>
2021-02-09 11:10:11 -0500

    added first clock attempt

---
+++ Clock/Clock.ino
@@ -0,0 +1,26 @@
+int latchPinH = 3;
+int clockPinH = 2;
+int dataPinH = 4;
+int latchPinM = 6;
+int clockPinM = 5;
+int dataPinM = 7;
+int latchPinS = 9;
+int clockPinS = 8;
+int dataPinS = 10;
+
+void setup() {
+  pinMode(latchPinS, OUTPUT);
+  pinMode(clockPinS, OUTPUT);
+  pinMode(dataPinS, OUTPUT);
+  pinMode(latchPinM, OUTPUT);
+  pinMode(clockPinM, OUTPUT);
+  pinMode(dataPinM, OUTPUT);
+  pinMode(latchPinH, OUTPUT);
+  pinMode(clockPinH, OUTPUT);
+  pinMode(dataPinH, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
