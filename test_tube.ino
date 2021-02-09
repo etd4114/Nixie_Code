5cd5cd1cddff6bb45b59542a611fa2d6b72109a5
Your Name <etd4sv@virgia.edu>
2021-02-09 10:49:44 -0500

    added initial files

---
+++ test_tube.ino
@@ -0,0 +1,104 @@
+/*int A = 2;
+int B = 3;
+int C = 4;
+int D = 5;
+int i = 0;
+int nums = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+}
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (true) {
+    if (i == 10) {
+      i = 0;
+    }
+    else {
+       nums = i;
+       switch (nums) {
+        case 1:
+        delay(1000);
+        digitalWrite(A, LOW);
+        digitalWrite(B, LOW);
+        digitalWrite(C, LOW);
+        digitalWrite(D, LOW);
+        break;
+        case 0:
+        delay(1000);
+        digitalWrite(A, HIGH);
+        digitalWrite(B, LOW);
+        digitalWrite(C, LOW);
+        digitalWrite(D, LOW);
+        break;
+        case 4:
+        delay(1000);
+        digitalWrite(A, LOW);
+        digitalWrite(B, HIGH);
+        digitalWrite(C, LOW);
+        digitalWrite(D, LOW);
+        break;
+        case 8:
+        delay(1000);
+        digitalWrite(A, HIGH);
+        digitalWrite(B, HIGH);
+        digitalWrite(C, LOW);
+        digitalWrite(D, LOW);
+        break;
+        case 2:
+        delay(1000);
+        digitalWrite(A, LOW);
+        digitalWrite(B, LOW);
+        digitalWrite(C, HIGH);
+        digitalWrite(D, LOW);
+        break;
+        case 6:
+        delay(1000);
+        digitalWrite(A, HIGH);
+        digitalWrite(B, LOW);
+        digitalWrite(C, HIGH);
+        digitalWrite(D, LOW);
+        break;
+        case 5:
+        delay(1000);
+        digitalWrite(A, LOW);
+        digitalWrite(B, HIGH);
+        digitalWrite(C, HIGH);
+        digitalWrite(D, LOW);
+        break;
+        case 9:
+        delay(1000);
+        digitalWrite(A, HIGH);
+        digitalWrite(B, HIGH);
+        digitalWrite(C, HIGH);
+        digitalWrite(D, LOW);
+        break;
+        case 3:
+        delay(1000);
+        digitalWrite(A, LOW);
+        digitalWrite(B, LOW);
+        digitalWrite(C, LOW);
+        digitalWrite(D, HIGH);
+        break;
+        case 7:
+        delay(1000);
+        digitalWrite(A, HIGH);
+        digitalWrite(B, LOW);
+        digitalWrite(C, LOW);
+        digitalWrite(D, HIGH);
+        break; 
+    }
+    i++;
+  }
+  
+
+  }
+    
+}*/
