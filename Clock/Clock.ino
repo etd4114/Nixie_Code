156f3afed57d1283d5d477665ffe4a0dc88a565e
Your Name <etd4sv@virgia.edu>
2021-02-09 19:07:52 -0500

    added clock and updated code

--- Clock/Clock.ino
+++ Clock/Clock.ino
@@ -1,12 +1,74 @@
-int latchPinH = 3;
-int clockPinH = 2;
-int dataPinH = 4;
-int latchPinM = 6;
-int clockPinM = 5;
-int dataPinM = 7;
-int latchPinS = 9;
-int clockPinS = 8;
-int dataPinS = 10;
+#include "MD_DS1307.h"
+
+#define latchPinH 3
+#define clockPinH 2
+#define dataPinH 4
+#define latchPinM 6
+#define clockPinM 5
+#define dataPinM 7
+#define latchPinS 9
+#define clockPinS 8
+#define dataPinS 10
+
+#define zero 0b1000 
+#define one 0b0000
+#define two 0b0010
+#define three 0b0001
+#define four 0b0100
+#define five 0b0110
+#define six 0b1010
+#define seven 0b1001
+#define eight 0b1100
+#define nine 0b1110
+
+int onesS;
+int tensS;
+int onesM;
+int tensM;
+int onesH;
+int tensH;
+
+byte byteDecode (int time) {
+  switch (time) {
+    case 0:
+      return zero;
+      break;
+    case 1:
+      return one;
+      break;
+    case 2:
+      return two;
+      break;
+    case 3:
+      return three;
+      break;
+    case 4:
+      return four;
+      break;
+    case 5:
+      return five;
+      break;
+    case 6:
+      return six;
+      break;
+    case 7:
+      return seven;
+      break; 
+    case 8:
+      return eight;
+      break;
+    case 9:
+      return nine;
+      break;
+  }
+}
+
+byte byteCombine (int timeOne, int timeTwo) {
+  byte t1 = byteDecode(timeOne);
+  byte t2 = byteDecode(timeTwo);
+  t1 = (t1 << 4);
+  return ((0b00000000 | t1) | t2);
+}
 
 void setup() {
   pinMode(latchPinS, OUTPUT);
@@ -22,5 +84,5 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
 }
